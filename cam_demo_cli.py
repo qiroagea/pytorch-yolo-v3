a8680c2fabe6385a220f2365e1f7fd0657990797
qiroagea <qiro@outlook.jp>
2019-12-22 16:29:46 +0900

    Update cam_demo_cli.py

--- cam_demo_cli.py
+++ cam_demo_cli.py
@@ -15,10 +15,8 @@ def get_test_input(input_dim, cuda):
     img_ = torch.from_numpy(img_).float()
     # noinspection PyArgumentList
     img_ = Variable(img_)
-
     if cuda:
         img_ = img_.cuda()
-
     return img_
 
 
@@ -28,7 +26,6 @@ def prep_img(image, inpDim):
     
     Returns a Variable 
     """
-
     origIm = image
     dimension = origIm.shape[1], origIm.shape[0]
     image = cv2.resize(origIm, (inpDim, inpDim))
@@ -52,18 +49,9 @@ def write(x, image):
 
 
 def write_cli(x):
-    # c1 = tuple(x[1:3].int())
-    # c2 = tuple(x[3:5].int())
     cls = int(x[-1])
     label = "{0}".format(classes[cls])
-    # color = random.choice(colors)
-    # cv2.rectangle(image, c1, c2, color, 1)
-    # t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
-    # c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-    # cv2.rectangle(image, c1, c2, color, -1)
-    # cv2.putText(image, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225, 255, 255], 1)
     print(label)
-    # return image
 
 
 def arg_parse():
@@ -71,7 +59,6 @@ def arg_parse():
     Parse arguements to the detect module
     
     """
-
     parser = argparse.ArgumentParser(description='YOLO v3 Cam Demo')
     parser.add_argument("--confidence", dest="confidence", help="Object Confidence to filter predictions", default=0.25)
     parser.add_argument("--nms_thresh", dest="nms_thresh", help="NMS Threshhold", default=0.4)
@@ -129,8 +116,6 @@ if __name__ == '__main__':
 
             img, orig_im, dim = prep_img(frame, inp_dim)
 
-            #            im_dim = torch.FloatTensor(dim).repeat(1,2)
-
             if CUDA:
                 # noinspection PyUnboundLocalVariable
                 im_dim = im_dim.cuda()
@@ -141,30 +126,20 @@ if __name__ == '__main__':
 
             if type(output) == int:
                 frames += 1
-                print("FPS of the video is {:5.2f}".format(frames / (time.time() - start)))
-                # cv2.imshow("frame", orig_im)
-                # key = cv2.waitKey(1)
-                # if key & 0xFF == ord('q'):
-                #     break
+                # print("FPS of the video is {:5.2f}".format(frames / (time.time() - start)))
                 continue
 
             output[:, 1:5] = torch.clamp(output[:, 1:5], 0.0, float(inp_dim)) / inp_dim
 
-            #            im_dim = im_dim.repeat(output.size(0), 1)
             output[:, [1, 3]] *= frame.shape[1]
             output[:, [2, 4]] *= frame.shape[0]
 
             classes = load_classes('data/coco.names')
             colors = pkl.load(open("pallete", "rb"))
 
-            # list(map(lambda x: write(x, orig_im), output))
             list(map(lambda x: write_cli(x), output))
 
-            # cv2.imshow("frame", orig_im)
-            # key = cv2.waitKey(1)
-            # if key & 0xFF == ord('q'):
-            #     break
             frames += 1
-            print("FPS of the video is {:5.2f}".format(frames / (time.time() - start)))
+            # print("FPS of the video is {:5.2f}".format(frames / (time.time() - start)))
         else:
             break
